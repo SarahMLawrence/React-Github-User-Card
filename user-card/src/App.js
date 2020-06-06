7d32e9e1b48ef1822ab7b5b2ecb890c2ac15dd2d
Sarah Lawrence <sarahlawrence1152@gmail.com>
2020-06-05 20:29:10 -0400

    stretch

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -2,14 +2,19 @@ import React from "react";
 import axios from "axios";
 
 import UserCard from "./components/UserCard";
-import FollowerCard from "./components/FollowerCard";
-import './App.css';
+import Search from "./components/Search";
+import "./App.css";
+
 class App extends React.Component {
   state = {
     user: {}, // DEFINING EMPTY ARRAY OF USER
     followers: [],
+    searchFollower: "",
   };
 
+  //==================//
+  // get user profile //
+  //==================//
   componentDidMount() {
     console.log("componentDidMount running");
     axios
@@ -19,6 +24,9 @@ class App extends React.Component {
       })
       .catch((err) => console.log(err));
 
+    //=========================//
+    // get followers profiles  //
+    //=========================//
     axios
       .get("https://api.github.com/users/SarahMLawrence/followers")
       .then((res) => {
@@ -27,28 +35,44 @@ class App extends React.Component {
       .catch((err) => console.log(err));
   }
 
+  handleChanges = (e) => {
+    console.log(e.target.name, e.target.value);
+    this.setState({ ...this.state, searchFollower: e.target.value });
+  };
+
+  fetchFollowers = (e) => {
+    e.preventDefault();
+    axios
+      .get(`https://api.github.com/users/${this.state.searchFollower}`)
+      .then((res) => this.setState({ ...this.state, followers: res.data }))
+      .catch((err) => console.log(err));
+  };
+
   render() {
     console.log("rendering");
     return (
       <div className="App">
+        <Search />
         <div>
-          <h1>Hello</h1>
           <UserCard
             login={this.state.user.login}
             avatar_url={this.state.user.avatar_url}
             html_url={this.state.user.html_url}
-            
+            followers={this.state.user.followers}
+            following={this.state.user.following}
           />
         </div>
+
         <div className="follower-card">
-          {this.state.followers.map((followers) => (
+          <h1>Followers</h1>
+          {this.state.followers.map((follower) => (
             <UserCard
-              
-              avatar_url={followers.avatar_url}
-              login={followers.login}
-              html_url={followers.html_url}
+              avatar_url={follower.avatar_url}
+              login={follower.login}
+              html_url={follower.html_url}
+              followers={this.state.user.followers}
+              following={this.state.user.following}
             />
-            
           ))}
         </div>
       </div>
--- user-card/src/components/UserCard.js
+++ user-card/src/components/UserCard.js
@@ -2,15 +2,27 @@ import React from "react";
 
 export default function Card(props) {
   return (
-    <div className="card">
-      <div className="img-wrapper">
-   
-   
-        <img src={props.avatar_url} alt={props.avatar_url} />
-        
-          <h1>{props.login}</h1>
-          <h1>{props.html_url}</h1>
-   
+    <div className="card-deck">
+      <div className="container">
+        <div className="card">
+          {/* <div className="img-wrapper"> */}
+
+          <img
+            className="card-img-top" //img
+            src={props.avatar_url}
+            alt={props.avatar_url}
+          />
+          <div class="card-body">
+            <h5 className="card-title">{props.login}</h5>
+            <p className="card-text">
+              {props.html_url}
+              <br></br>
+              Followers: {props.followers}
+              <br></br>
+              Following: {props.following}
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
